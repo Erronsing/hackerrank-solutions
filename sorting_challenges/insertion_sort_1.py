1deaceee4ab67f763a816a687f0f6eac4c31a5a6
Garth <garthbjerk@gmail.com>
2015-11-22 16:37:29 -0700

    first sorting challenge
    Trying to get insertion sort 1 working

---
+++ sorting_challenges/insertion_sort_1.py
@@ -0,0 +1,14 @@
+#!/bin/python
+def insertionSort(ar):
+    e = ar[m-1]
+    for i in xrange(m-2, -1, -1):
+        if ar[i]>e:
+            ar[i+1] = ar[i]
+        else:
+            ar[i+1] = e
+        print ' '.join(str(x) for x in ar)
+    return ""
+
+m = input()
+ar = [int(i) for i in raw_input().strip().split()]
+insertionSort(ar)
